c79cfe974f3007137760a500907edf8ee741115c
rohit dixit <iamrohitdixit17@gmail.com>
2025-10-24 18:22:23 +0530

    C++1

---
+++ Desktop/c++/third.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"The value of num 1 is:\n"; /*'<<' is called Insertion Operator*/
+    cin>>num1; /*'>>' is called Extraction operator*/
+    cout<<"The value of num 2 is:\n"; /*'<<' is called Insertion Operator*/
+    cin>>num2; /*'>>' is called Extraction operator*/
+    cout<<"The sum is "<<num1+num2;
+    return 0;
+}
